7af111a9c65d8566b0713fdee7d6915fa5d3c79d
akkamaddi <akkamaddi@hotmail.com>
2014-01-20 12:08:33 -0500

    20140120

--- 1.3.B/akkamaddi/ClassicalAlchemy/code/HandlerJoinWorld.java
+++ 1.3.B/akkamaddi/ClassicalAlchemy/code/HandlerJoinWorld.java
@@ -23,10 +23,10 @@ public class HandlerJoinWorld
     {
 
         rand = Math.random();
-        range = random.nextInt(1, 15);
+        range = random.nextInt(1, 81);
         
 
-        if ((rand <= 0.04D) && ((event.entity instanceof EntityLiving)) && ((event.entity instanceof EntitySkeleton | event.entity instanceof EntityZombie)))
+        if ((rand <= 0.02D) && ((event.entity instanceof EntityLiving)) && ((event.entity instanceof EntitySkeleton | event.entity instanceof EntityZombie)))
         {
             EntityLiving living = (EntityLiving)event.entity;
 
@@ -100,14 +100,12 @@ public class HandlerJoinWorld
                 living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.stannumPickaxe));
             }
 
-
             if (range == 13)
             {
                 living.setCurrentItemOrArmor(4, new ItemStack(ClassicalAlchemyCore.stannumHelm));
                 living.setCurrentItemOrArmor(3, new ItemStack(ClassicalAlchemyCore.stannumChest));
                 living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.stannumAxe));
             }
-
 
             if (range == 14)
             {
@@ -116,17 +114,335 @@ public class HandlerJoinWorld
                 living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.stannumShovel));
             }
 
-
             if (range == 15)
             {
                 living.setCurrentItemOrArmor(4, new ItemStack(ClassicalAlchemyCore.stannumHelm));
                 living.setCurrentItemOrArmor(1, new ItemStack(ClassicalAlchemyCore.stannumBoots));
                 living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.stannumHoe));
             }
+            
+            if (range == 16)
+            {
+                living.setCurrentItemOrArmor(4, new ItemStack(ClassicalAlchemyCore.cuprumHelm));
+                living.setCurrentItemOrArmor(3, new ItemStack(ClassicalAlchemyCore.cuprumChest));
+                living.setCurrentItemOrArmor(2, new ItemStack(ClassicalAlchemyCore.cuprumLegs));
+                living.setCurrentItemOrArmor(1, new ItemStack(ClassicalAlchemyCore.cuprumBoots));
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.cuprumSword));
+            }
 
- 
- 
-        }
+            if (range == 17)
+            {
+                living.setCurrentItemOrArmor(4, new ItemStack(ClassicalAlchemyCore.cuprumHelm));
+            }
+
+            if (range == 18)
+            {
+                living.setCurrentItemOrArmor(3, new ItemStack(ClassicalAlchemyCore.cuprumChest));
+            }
+
+            if (range == 19)
+            {
+                living.setCurrentItemOrArmor(2, new ItemStack(ClassicalAlchemyCore.cuprumLegs));
+            }
+
+            if (range == 20)
+            {
+                living.setCurrentItemOrArmor(1, new ItemStack(ClassicalAlchemyCore.cuprumBoots));
+            }
+
+            if (range == 21)
+            {
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.cuprumSword));
+            }
+
+            if (range == 22)
+            {
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.cuprumShovel));
+            }
+
+            if (range == 23)
+            {
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.cuprumAxe));
+            }
+
+            if (range == 24)
+            {
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.cuprumPickaxe));
+            }
+
+            if (range == 25)
+            {
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.cuprumHoe));
+            }
+            
+            if (range == 26)
+            {
+                living.setCurrentItemOrArmor(4, new ItemStack(ClassicalAlchemyCore.cuprumHelm));
+                living.setCurrentItemOrArmor(3, new ItemStack(ClassicalAlchemyCore.cuprumChest));
+                living.setCurrentItemOrArmor(2, new ItemStack(ClassicalAlchemyCore.cuprumLegs));
+                living.setCurrentItemOrArmor(1, new ItemStack(ClassicalAlchemyCore.cuprumBoots));
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.cuprumSword));
+            }
+
+            if (range == 27)
+            {
+                living.setCurrentItemOrArmor(4, new ItemStack(ClassicalAlchemyCore.cuprumHelm));
+                living.setCurrentItemOrArmor(1, new ItemStack(ClassicalAlchemyCore.cuprumBoots));
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.cuprumPickaxe));
+            }
+
+            if (range == 28)
+            {
+                living.setCurrentItemOrArmor(4, new ItemStack(ClassicalAlchemyCore.cuprumHelm));
+                living.setCurrentItemOrArmor(3, new ItemStack(ClassicalAlchemyCore.cuprumChest));
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.cuprumAxe));
+            }
+
+            if (range == 29)
+            {
+                living.setCurrentItemOrArmor(2, new ItemStack(ClassicalAlchemyCore.cuprumLegs));
+                living.setCurrentItemOrArmor(1, new ItemStack(ClassicalAlchemyCore.cuprumBoots));
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.cuprumShovel));
+            }
+
+            if (range == 30)
+            {
+                living.setCurrentItemOrArmor(4, new ItemStack(ClassicalAlchemyCore.cuprumHelm));
+                living.setCurrentItemOrArmor(1, new ItemStack(ClassicalAlchemyCore.cuprumBoots));
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.cuprumHoe));
+            }
+
+            if (range == 31)
+            {
+                living.setCurrentItemOrArmor(4, new ItemStack(ClassicalAlchemyCore.pyropusBronzeHelm));
+                living.setCurrentItemOrArmor(3, new ItemStack(ClassicalAlchemyCore.pyropusBronzeChest));
+                living.setCurrentItemOrArmor(2, new ItemStack(ClassicalAlchemyCore.pyropusBronzeLegs));
+                living.setCurrentItemOrArmor(1, new ItemStack(ClassicalAlchemyCore.pyropusBronzeBoots));
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.pyropusBronzeSword));
+            }
+
+            if (range == 32)
+            {
+                living.setCurrentItemOrArmor(4, new ItemStack(ClassicalAlchemyCore.pyropusBronzeHelm));
+            }
+
+            if (range == 33)
+            {
+                living.setCurrentItemOrArmor(3, new ItemStack(ClassicalAlchemyCore.pyropusBronzeChest));
+            }
+
+            if (range == 34)
+            {
+                living.setCurrentItemOrArmor(2, new ItemStack(ClassicalAlchemyCore.pyropusBronzeLegs));
+            }
+
+            if (range == 35)
+            {
+                living.setCurrentItemOrArmor(1, new ItemStack(ClassicalAlchemyCore.pyropusBronzeBoots));
+            }
+
+            if (range == 36)
+            {
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.pyropusBronzeSword));
+            }
+
+            if (range == 37)
+            {
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.pyropusBronzeShovel));
+            }
+
+            if (range == 38)
+            {
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.pyropusBronzeAxe));
+            }
+
+            if (range == 39)
+            {
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.pyropusBronzePickaxe));
+            }
+
+            if (range == 40)
+            {
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.pyropusBronzeHoe));
+            }
+            
+            if (range == 41)
+            {
+                living.setCurrentItemOrArmor(4, new ItemStack(ClassicalAlchemyCore.pyropusBronzeHelm));
+                living.setCurrentItemOrArmor(3, new ItemStack(ClassicalAlchemyCore.pyropusBronzeChest));
+                living.setCurrentItemOrArmor(2, new ItemStack(ClassicalAlchemyCore.pyropusBronzeLegs));
+                living.setCurrentItemOrArmor(1, new ItemStack(ClassicalAlchemyCore.pyropusBronzeBoots));
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.pyropusBronzeSword));
+            }
+
+            if (range == 42)
+            {
+                living.setCurrentItemOrArmor(4, new ItemStack(ClassicalAlchemyCore.pyropusBronzeHelm));
+                living.setCurrentItemOrArmor(1, new ItemStack(ClassicalAlchemyCore.pyropusBronzeBoots));
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.pyropusBronzePickaxe));
+            }
+
+            if (range == 43)
+            {
+                living.setCurrentItemOrArmor(4, new ItemStack(ClassicalAlchemyCore.pyropusBronzeHelm));
+                living.setCurrentItemOrArmor(3, new ItemStack(ClassicalAlchemyCore.pyropusBronzeChest));
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.pyropusBronzeAxe));
+            }
+
+            if (range == 44)
+            {
+                living.setCurrentItemOrArmor(2, new ItemStack(ClassicalAlchemyCore.pyropusBronzeLegs));
+                living.setCurrentItemOrArmor(1, new ItemStack(ClassicalAlchemyCore.pyropusBronzeBoots));
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.pyropusBronzeShovel));
+            }
+
+            if (range == 45)
+            {
+                living.setCurrentItemOrArmor(4, new ItemStack(ClassicalAlchemyCore.pyropusBronzeHelm));
+                living.setCurrentItemOrArmor(1, new ItemStack(ClassicalAlchemyCore.pyropusBronzeBoots));
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.pyropusBronzeHoe));
+            }
+            
+            if (range == 46)
+            {
+                living.setCurrentItemOrArmor(4, new ItemStack(ClassicalAlchemyCore.pyropusBronzeHelm));
+                living.setCurrentItemOrArmor(3, new ItemStack(ClassicalAlchemyCore.pyropusBronzeChest));
+                living.setCurrentItemOrArmor(2, new ItemStack(ClassicalAlchemyCore.pyropusBronzeLegs));
+                living.setCurrentItemOrArmor(1, new ItemStack(ClassicalAlchemyCore.pyropusBronzeBoots));
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.pulchrumBronzeSword));
+            }
+
+            if (range == 47)
+            {
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.pulchrumBronzeSword));
+            }
+
+            if (range == 48)
+            {
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.pulchrumBronzeShovel));
+            }
+
+            if (range == 49)
+            {
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.pulchrumBronzeAxe));
+            }
+
+            if (range == 50)
+            {
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.pulchrumBronzePickaxe));
+            }
+
+            if (range == 51)
+            {
+                living.setCurrentItemOrArmor(4, new ItemStack(ClassicalAlchemyCore.pyropusBronzeHelm));
+                living.setCurrentItemOrArmor(3, new ItemStack(ClassicalAlchemyCore.pyropusBronzeChest));
+                living.setCurrentItemOrArmor(2, new ItemStack(ClassicalAlchemyCore.pyropusBronzeLegs));
+                living.setCurrentItemOrArmor(1, new ItemStack(ClassicalAlchemyCore.pyropusBronzeBoots));
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.pulchrumBronzeSword));
+            }
+
+            if (range == 52)
+            {
+                living.setCurrentItemOrArmor(4, new ItemStack(ClassicalAlchemyCore.pyropusBronzeHelm));
+                living.setCurrentItemOrArmor(1, new ItemStack(ClassicalAlchemyCore.pyropusBronzeBoots));
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.pulchrumBronzePickaxe));
+            }
+
+            if (range == 53)
+            {
+                living.setCurrentItemOrArmor(4, new ItemStack(ClassicalAlchemyCore.pyropusBronzeHelm));
+                living.setCurrentItemOrArmor(3, new ItemStack(ClassicalAlchemyCore.pyropusBronzeChest));
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.pulchrumBronzeAxe));
+            }
+
+            if (range == 54)
+            {
+                living.setCurrentItemOrArmor(2, new ItemStack(ClassicalAlchemyCore.pyropusBronzeLegs));
+                living.setCurrentItemOrArmor(1, new ItemStack(ClassicalAlchemyCore.pyropusBronzeBoots));
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.pulchrumBronzeShovel));
+            }
+            
+
+            if (range == 55 ||range == 56 || range == 57)
+            {
+                living.setCurrentItemOrArmor(4, new ItemStack(ClassicalAlchemyCore.tombBronzeHelm));
+                living.setCurrentItemOrArmor(3, new ItemStack(ClassicalAlchemyCore.tombBronzeChest));
+                living.setCurrentItemOrArmor(2, new ItemStack(ClassicalAlchemyCore.tombBronzeLegs));
+                living.setCurrentItemOrArmor(1, new ItemStack(ClassicalAlchemyCore.tombBronzeBoots));
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.tombBronzeSword));
+            }
+
+            if (range == 58 || range == 59)
+            {
+                living.setCurrentItemOrArmor(4, new ItemStack(ClassicalAlchemyCore.tombBronzeHelm));
+            }
+
+            if (range == 60 || range == 61)
+            {
+                living.setCurrentItemOrArmor(3, new ItemStack(ClassicalAlchemyCore.tombBronzeChest));
+            }
+
+            if (range == 62 || range == 63)
+            {
+                living.setCurrentItemOrArmor(2, new ItemStack(ClassicalAlchemyCore.tombBronzeLegs));
+            }
+
+            if (range == 64 || range == 65)
+            {
+                living.setCurrentItemOrArmor(1, new ItemStack(ClassicalAlchemyCore.tombBronzeBoots));
+            }
+
+            if (range == 66 || range == 67)
+            {
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.tombBronzeSword));
+            }
+
+            if (range == 68 || range == 69)
+            {
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.tombBronzeShovel));
+            }
+
+            if (range == 70 || range == 71)
+            {
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.tombBronzeAxe));
+            }
+
+            if (range == 72 || range == 73)
+            {
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.tombBronzePickaxe));
+            }
+
+            if (range == 74 || range == 75)
+            {
+                living.setCurrentItemOrArmor(4, new ItemStack(ClassicalAlchemyCore.tombBronzeHelm));
+                living.setCurrentItemOrArmor(3, new ItemStack(ClassicalAlchemyCore.tombBronzeChest));
+                living.setCurrentItemOrArmor(2, new ItemStack(ClassicalAlchemyCore.tombBronzeLegs));
+                living.setCurrentItemOrArmor(1, new ItemStack(ClassicalAlchemyCore.tombBronzeBoots));
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.tombBronzeSword));
+            }
+
+            if (range == 76 || range == 77)
+            {
+                living.setCurrentItemOrArmor(4, new ItemStack(ClassicalAlchemyCore.tombBronzeHelm));
+                living.setCurrentItemOrArmor(1, new ItemStack(ClassicalAlchemyCore.tombBronzeBoots));
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.tombBronzePickaxe));
+            }
+
+            if (range == 78 || range == 79)
+            {
+                living.setCurrentItemOrArmor(4, new ItemStack(ClassicalAlchemyCore.tombBronzeHelm));
+                living.setCurrentItemOrArmor(3, new ItemStack(ClassicalAlchemyCore.tombBronzeChest));
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.tombBronzeAxe));
+            }
+
+            if (range == 80 || range == 81)
+            {
+                living.setCurrentItemOrArmor(2, new ItemStack(ClassicalAlchemyCore.tombBronzeLegs));
+                living.setCurrentItemOrArmor(1, new ItemStack(ClassicalAlchemyCore.tombBronzeBoots));
+                living.setCurrentItemOrArmor(0, new ItemStack(ClassicalAlchemyCore.tombBronzeShovel));
+            }
+            
+         }
     }
 }
 
---
+++ 1.3.B/akkamaddi/ClassicalAlchemy/code/SimpleArsenicContents.java
@@ -0,0 +1,33 @@
+package akkamaddi.ClassicalAlchemy.code;
+
+import net.minecraft.item.Item;
+
+public class SimpleArsenicContents extends ModContents {
+	
+	public Class
+	items;
+
+	public Content<Item>
+	arsenideBronzeIngot,
+	oldLaceChest;
+	
+	
+	public SimpleArsenicContents() {
+		if (!isModLoaded())
+			return;
+		
+		try {
+			items = Class.forName("akkamaddi.arsenic.code.ArsenicAndLace");
+		} catch (ClassNotFoundException e) {
+			throw new ModReflectionFailException(e);
+		}
+		
+		arsenideBronzeIngot = of(items, "arsenideBronzeIngot");
+		oldLaceChest = of(items, "oldLaceChest");
+	}
+	
+	@Override public String getModId() {
+		return "SimpleArsenic";
+	}
+	
+}
